723f7fcf369ac7674602ca19ba1e671779ab6b59
Restuadil15 <73323256+Restuadil15@users.noreply.github.com>
2022-01-04 12:58:38 +0700

    Add files via upload

---
+++ praktikum_09/app.js
@@ -0,0 +1,27 @@
+/**
+ * TODO 9:
+ * - Import semua method FruitController
+ * - Refactor variable ke ES6 Variable
+ *
+ * @hint - Gunakan Destructing Object
+ */
+
+    const {index,store,update,destroy} = require("./FruitController");
+
+/**
+  * NOTES:
+  * - Fungsi main tidak perlu diubah
+  * - Jalankan program: nodejs app.js
+  */
+const main = () => {
+    console.log("Method index - Menampilkan Buah");
+    index();
+    console.log("\nMethod store - Menambahkan buah Anggur");
+    store("Anggur");
+    console.log("\nMethod update - Update data 0 menjadi Nanas");
+    update(0, "Nanas");
+    console.log("\nMethod destroy - Menghapus data 0");
+    destroy(0);
+};
+
+main();
---
+++ praktikum_09/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["Anggur","Nanas","Jeruk"];
+
+ // TODO 2: export variable fruits
+module.exports = fruits;
